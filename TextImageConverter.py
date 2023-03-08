89101f146644edee00f5bc8bb50aff02c7925f83
Harrisonust <henhen.luo@gmail.com>
2023-03-08 18:26:28 +0800

    refacotr by chatgpt

--- TextImageConverter.py
+++ TextImageConverter.py
@@ -1,45 +1,37 @@
 import cv2
-import numpy
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-
-
-def cv2ImgAddText(img, text, left, top, textColor=(0, 0, 0), textSize=15):
-    if (isinstance(img, numpy.ndarray)):  # 判斷是否OpenCV圖片類型
+def add_text_to_image(img, text, left, top, text_color=(0, 0, 0), text_size=15):
+    if isinstance(img, np.ndarray):  
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(img)
-    fontText = ImageFont.truetype(
-        "font/simsun.ttc", textSize, encoding="utf-8")
-    draw.text((left, top), text, textColor, font=fontText)
-    return cv2.cvtColor(numpy.asarray(img), cv2.COLOR_RGB2BGR)
+    font_text = ImageFont.truetype("font/simsun.ttc", text_size, encoding="utf-8")
+    draw.text((left, top), text, text_color, font=font_text)
+    return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
 
-def wordConvert2mat(img):
-    img = cv2ImgAddText(img, targetWord, 0, 0)
+def convert_word_to_matrix(target_word, rsl):
+    img = np.zeros((rsl[0], rsl[1], 1), np.uint8)
+    img.fill(255)
+    img = add_text_to_image(img, target_word, 0, 0)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     ret, img = cv2.threshold(img, 210, 255, cv2.THRESH_BINARY)
-    img = cv2.resize(img, RSL)
-
+    img = cv2.resize(img, rsl)
     return img
 
 
-while(1):
-    RSL = (15, 15)
-    targetWords = input("轉換字：")
+while True:
+    rsl = (15, 15)
+    target_words = input("Texts to be converted: ")
 
-    for i in range(len(targetWords)):
-        img = numpy.zeros((RSL[0], RSL[1], 1), numpy.uint8)
-        img.fill(255)
-        targetWord = targetWords[i]
+    for target_word in target_words:
+        img = convert_word_to_matrix(target_word, rsl)
 
-        img = wordConvert2mat(img)
-
-        ary = []
-        for i in range(RSL[0]):
-            for j in range(RSL[1]):
+        for i in range(rsl[0]):
+            for j in range(rsl[1]):
                 if img[i][j] == 255:
                     print(chr(12288), end='')
                 else:
-                    print(targetWord, end='')
+                    print(target_word, end='')
             print()
-    
