f86f21c7644fc3ac4a5e574c60661b26f7c4e107
Harrisonust <henhen.luo@gmail.com>
2023-03-09 12:59:28 +0800

    drop down menu for output size selection; backlink icon; modify UI

--- server/aws-lambda.py
+++ server/aws-lambda.py
@@ -23,43 +23,39 @@ def add_text_to_image(img, text, left, top, text_color=(0, 0, 0), text_size=15):
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
 
-def convert_word_to_matrix(target_word, rsl):
-    img = np.zeros((rsl[0], rsl[1], 1), np.uint8)
+def convert_word_to_matrix(target_word, width):
+    img = np.zeros((width, width, 1), np.uint8)
     img.fill(255)
-    img = add_text_to_image(img, target_word, 0, 0)
+    img = add_text_to_image(img, target_word, 0, 0, text_size=width)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     ret, img = cv2.threshold(img, 210, 255, cv2.THRESH_BINARY)
-    img = cv2.resize(img, rsl)
+    img = cv2.resize(img, (width, width))
     return img
 
 def lambda_handler(event, context):
-    rsl = (15, 15)
-    
-
     print(f"{event=}")
     print(f"{context=}")
     
     body = event['body'] # assuming the JSON data is in the "body" field of the event
-    
-    if body is None:
-        target_words = "空"
-    else:
+    width = 15
+    target_words = "空"
+    if body is not None:
         data = json.loads(body)
         target_words = data.get('target', "空")
-        
-    result = np.empty((len(target_words), 15, 15), int)
+        width = data.get('width', 15)
+    
+    result = np.empty((len(target_words), width, width), int)
 
     for index, target_word in enumerate(target_words):
-        img = convert_word_to_matrix(target_word, rsl)
+        img = convert_word_to_matrix(target_word, width)
         
-        for i in range(rsl[0]):
-            for j in range(rsl[1]):
+        for i in range(width):
+            for j in range(width):
                 if img[i][j] == 255:
                     print(chr(12288), end='')
                     result[index][i][j] = 0
                 else:
                     print(target_word, end='') 
-                    # result[i][j] = target_word.encode('unicode_escape')
                     result[index][i][j] = 1
             print()
     result = result.tolist()
