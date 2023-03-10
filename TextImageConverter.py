4260b89b68da32060e13a54afb3a8dfd46577208
Harrisonust <henhen.luo@gmail.com>
2023-03-10 11:44:00 +0800

    align with lambda

--- TextImageConverter.py
+++ TextImageConverter.py
@@ -1,13 +1,15 @@
+import json
 import cv2
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
-ascii = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 def add_text_to_image(img, text, left, top, text_color=(0, 0, 0), text_size=15):
     if isinstance(img, np.ndarray):  
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(img)
-    if text in ascii:
+    if text in ascii_letters:
         font_text = ImageFont.truetype("font/arial.ttf", text_size)
     else:
         font_text = ImageFont.truetype("font/simsun.ttc", text_size, encoding="utf-8")
@@ -15,27 +17,30 @@ def add_text_to_image(img, text, left, top, text_color=(0, 0, 0), text_size=15):
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 
 
-def convert_word_to_matrix(target_word, rsl):
-    img = np.zeros((rsl[0], rsl[1], 1), np.uint8)
+def convert_word_to_matrix(target_word, width):
+    img = np.zeros((width, width, 1), np.uint8)
     img.fill(255)
-    img = add_text_to_image(img, target_word, 0, 0)
+    img = add_text_to_image(img, target_word, 0, 0, text_size=width)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     ret, img = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
-    img = cv2.resize(img, rsl)
+    img = cv2.resize(img, (width, width))
     return img
 
-
 while True:
-    rsl = (15, 15)
+    width = 15
     target_words = input("Texts to be converted: ")
+    
+    result = np.empty((len(target_words), width, width), int)
 
-    for target_word in target_words:
-        img = convert_word_to_matrix(target_word, rsl)
-
-        for i in range(rsl[0]):
-            for j in range(rsl[1]):
+    for index, target_word in enumerate(target_words):
+        img = convert_word_to_matrix(target_word, width)
+        
+        for i in range(width):
+            for j in range(width):
                 if img[i][j] == 255:
                     print(chr(12288), end='')
+                    result[index][i][j] = 0
                 else:
-                    print(target_word, end='')
+                    print(target_word, end='') 
+                    result[index][i][j] = 1
             print()
